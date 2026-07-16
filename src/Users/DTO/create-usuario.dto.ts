d9968da88635ee66cdadbc5778eabfa78b420609
Gvrh05 <delizondogutierrez@gmail.com>
2026-07-16 14:57:36 -0600

    Agrega regla de validación en create-usuario.dto

--- src/Users/DTO/create-usuario.dto.ts
+++ src/Users/DTO/create-usuario.dto.ts
@@ -1,6 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 export class CreateUsuarioDto {
+  @IsString()
+  @IsNotEmpty()
   nombre: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
   confirmPassword: string;
 }
